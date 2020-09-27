64c49cad60b18650038718a5ce1738544356b540
natys6ka <natalkapni.bc@gmail.com>
2020-09-27 22:32:28 +0300

    Exsersize 2

--- script.js
+++ script.js
@@ -25,3 +25,48 @@ console.log(worker.secondName); //виведе 'Ivanov'
 console.log(worker.rate); //виведе 10
 console.log(worker.days); //виведе 31
 console.log(worker.getSalary()); //виведе 310 - тобто 10*31
+
+
+/* Завдання 2.
+Реалізуйте клас MyString, який матиме наступні методи: 
+метод reverse(), який параметром приймає рядок, 
+    а повертає її в перевернутому вигляді, 
+метод ucFirst(), який параметром приймає рядок, 
+    а повертає цю ж рядок, зробивши її першу букву заголовної 
+та метод ucWords(), який приймає рядок 
+    і робить капіталізації першої літери кожного слова цього рядка.
+ */
+
+ class MyString{
+    constructor(text){
+        this.text = text
+    }
+    reverse(text){
+        // let reverseString = "";
+        // for (let i = text.length - 1; i >= 0; i--) {
+        //     reverseString += text[i];
+        // }
+        // return reverseString;
+        return text.split("").reverse().join("");
+    }
+    ucFirst(text){
+        return text.charAt(0).toUpperCase() + text.slice(1);
+    }
+    ucWords(text){
+        // let ucWordsStr = text.toLowerCase().split(' ');
+        // for (let i = 0; i < ucWordsStr.length; i++) {
+        //     ucWordsStr[i] = ucWordsStr[i].charAt(0).toUpperCase() + ucWordsStr[i].substring(1);     
+        // }
+        // return ucWordsStr.join(' '); 
+        return text.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase()+word.slice(1)).join(' ');
+    }
+
+ }
+
+console.log('Завдання 2');
+
+const str = new MyString();
+
+console.log(str.reverse('IVAN')); //виведе 'NAVI'
+console.log(str.ucFirst('arsenal')); //виведе 'Arsenal'
+console.log(str.ucWords('arsenal arsenal arsenal')); //виведе 'Arsenal Arsenal Arsenal'
